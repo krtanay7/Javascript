e585c2e23071b9aa6dd56d09973c304bf4634d93
Kumar Tanay <krtanay97@gmail.com>
2025-03-20 11:08:52 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -36,4 +36,51 @@ console.log(stringNum); // output = 45
 console.log(typeof stringNum); // output = string
 
 
+// *********************** Operations **********************
 
+let value = 3 
+let negValue = -value // conversion to positive to negative value 
+console.log(negValue);
+
+//basic mathematics:-  
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+// addition of strings
+let str1 = "Hello!"
+let str2 = " Tanay"
+let str3 = str1+ str2
+console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+""); // + Null
+
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter1 = 100
+let gameCounter2 = 100
+++gameCounter1;
+gameCounter2++;
+console.log(gameCounter1);
+console.log(gameCounter2);
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
