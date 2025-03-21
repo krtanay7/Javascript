711334eed5ecc38d9fc6c66eadf71e9df94cd121
Kumar Tanay <krtanay97@gmail.com>
2025-03-21 20:49:11 +0000

    for loop

--- 05_loops_iterations/one.js
+++ 05_loops_iterations/one.js
@@ -24,7 +24,7 @@ for (let index = 0; index < myArray.length; index++) {
     const element = myArray[index];
     //console.log(element);
     }
-
+//************************************************************************************************* */
 // break & continue 
 
 for (let index = 1; index <= 20; index++) {
