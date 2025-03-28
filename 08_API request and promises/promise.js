b910e189a9e3e754d48788d555933a4f87d99398
Kumar Tanay <krtanay97@gmail.com>
2025-03-28 15:33:08 +0000

    promises

---
+++ 08_API request and promises/promise.js
@@ -0,0 +1,13 @@
+console.log("promises");
+
+//The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+
+//A Promise is in one of these states:
+
+            // pending: initial state, neither fulfilled nor rejected.
+            // fulfilled: meaning that the operation was completed successfully.
+            // rejected: meaning that the operation failed.
+
+
+// A Promise in JavaScript is a way to handle asynchronous operations. 
+// This means when a task takes time to complete (like fetching data from an API, reading a file, or database queries), instead of waiting for it, JavaScript moves on and handles it using Promises.
